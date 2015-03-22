9c339a422e2b06a88bb9db955a52a5cbdf077fa2
yssh <yssh@example.com>
2015-03-22 18:18:35 +0900

    - Userモデルの追加  - 基本的なレイアウトの作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root to: 'home#index'
 
+  devise_for :users
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
